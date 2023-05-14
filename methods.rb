fc6ff0009ae4914f4c9d25950f84671c1b3fa24b
cliff <cliffmudigo5@gmail.com>
2023-05-14 23:05:49 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  def greet(name = "Naureen")
+   puts "Hello, #{name}!"
+  end
+  
+def greet_with_default (name = "programmer")
+  puts "Hello, #{name}!"
+end
+  
+def add(a, b)
+  a + b
+end
+
+def halve(number)
+  return number / 2 if number.is_a?(Integer)
+  return nil
+end
+
